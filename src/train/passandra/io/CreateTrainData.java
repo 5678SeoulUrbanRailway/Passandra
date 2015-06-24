0b76d348b379f029694ca9e0e36db259e3af4572
Saehyun Kim <saehyun91@gmail.com>
2015-06-24 21:01:38 +0900

    add checkStringType()

--- src/train/passandra/io/CreateTrainData.java
+++ src/train/passandra/io/CreateTrainData.java
@@ -1,4 +1,5 @@
 package train.passandra.io;
+
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,8 +10,8 @@ import org.jsoup.select.Elements;
 
 public class CreateTrainData {
 	private static CreateTrainData instance;
-	
-	private String wikiUri;
+
+	private String wikiEngUri, wikiKorUri;
 	private String regexTag, regexBraket, regexEng;
 
 	public static synchronized CreateTrainData getInstance() {
@@ -21,7 +22,8 @@ public class CreateTrainData {
 	}
 
 	private CreateTrainData() {
-		this.wikiUri = "https://en.wikipedia.org/?title=";
+		this.wikiEngUri = "https://en.wikipedia.org/?title=";
+		this.wikiKorUri = "https://ko.wikipedia.org/wiki/";
 		this.regexTag = "<[^>]*>"; // html tag 제거
 		this.regexBraket = "[\\[\\d\\]]"; // [%d] 제거
 		this.regexEng = "[^\\x00-\\x7F]"; // 영어 이외에 문자 제거
@@ -46,13 +48,31 @@ public class CreateTrainData {
 	}
 
 	/**
+	 * 검색어 search의 타입 판별
+	 * @param search
+	 * @return 한글이면 F, 영어면 T
+	 */
+	private boolean checkStringType(String search) {
+		for (int i = 0; i < search.length(); i++) {
+			if (Character.getType(search.charAt(i)) == Character.OTHER_LETTER) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
 	 * 웹 사이트에서 search에 대한 내용을 파싱하여 String형태로 반환한다.
 	 * @param 검색할 내용
 	 * @return search의 대한 검색 결과
 	 */
 	public String getContent(String search) {
 		try {
-			Document doc = Jsoup.connect(wikiUri + search).userAgent("Mozilla/5.0").get();
+			String uri = wikiKorUri;
+			if (checkStringType(search)) {
+				uri = wikiEngUri;
+			}
+			Document doc = Jsoup.connect(uri + search).userAgent("Mozilla/5.0").get();
 			Elements selectByTag = doc.select("li"); // li 태그 내용 선택
 
 			String content = selectByTag.html(); // 해당 태그 제거
